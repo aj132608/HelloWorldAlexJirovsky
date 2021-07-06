30db7154abe8010f144e03caf4fad1a6059b7b4d
Alex Jirovsky <aj132608@gmail.com>
2021-07-06 00:28:23 -0500

    Minor feature change

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,13 @@
 class HelloWorld:
-    def hello(self):
-        return "hello world"
+    def hello(self, num):
+        message = ""
+
+        for _ in range(num):
+            message += "hello world!"
+
+        return message
+
+
+if __name__ == '__main__':
+    hw_obj = HelloWorld()
+    print(hw_obj.hello(2))
